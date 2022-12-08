8ec6a1f1439286ca1c570f49d72dc39979afb1d9
Lego Kapi Man <kahanus2006@gmail.com>
2022-12-08 21:49:30 +0100

    commit

--- script.js
+++ script.js
@@ -2,54 +2,53 @@ const btn = document.querySelector(".spin");
 const wheel = document.querySelector(".box");
 const inputs = document.querySelectorAll("input");
 const spans = document.querySelectorAll("div.span div");
+const result = document.querySelector("#result");
 const arrow = document.querySelector(".mainbox");
+let panelIndex = [0, 0];
 
 let panels = [
-    "pink",
-    "green",
-    "blue",
-    "pink",
-    "yellow",
-    "blue",
-    "green"
+    ["pink", 337.6, 22.5, 1],
+    ["green", 22.6, 67.5, 2],
+    ["blue", 67.6, 112.5, 3],
+    ["pink", 112.6, 157.5, 1],
+    ["yellow", 157.6, 202.5, 0],
+    ["blue", 202.6, 247.5, 3],
+    ["green", 247.6, 292.5, 2],
+    ["yellow", 292.6, 337.5, 0]
 ];
 
-let rotation = 0;
-let lastRotation = 0;
-let panel = 0;
-let newPanel = null;
-
-let fullRotations = 0;
-let innerRotation = 45;
 
 btn.addEventListener("click", () => {
-    lastRotation = rotation;
-    let deg = Math.floor(Math.random() * 1800);
-    // let deg = 200;
+    result.innerHTML = "";
+
+    // wheel rotation
+    let deg = Math.floor(Math.random() * 3000);
     wheel.style.transform = `rotate(${deg}deg)`;
-    rotation = deg;
-    let difference = rotation - lastRotation;
 
-    // console.log(lastRotation, rotation);
-    // console.log(`roznica: ${difference}`);
-    // console.log(`end: ${difference % 45}`);
-    // console.log(`panels: ${Math.floor(difference / 45)}`);
+    // check spin result
+    let rotation = deg % 360;
+    for (let i = 0; i < panels.length; i++) {
+        if (rotation >= panels[i][1] && rotation <= panels[i][2]) {
+            panelIndex[0] = i;
+            panelIndex[1] = panels[panelIndex[0]][3];
+            break;
+        }
+    }
 
-    panel = panel + Math.floor(Math.abs(difference / 45));
-    console.log("panel index: " + panel);
-    if (panel > 8) { panel = panel % 8; }
-    console.log("panel index sub: " + panel);
-
-    console.log(panels[panel - 1]);
-
-    // fullRotations = Math.floor(deg / 360);
-    // innerRotation = rotation - ((rotation % 45) * fullRotations)
-    // console.log(rotation, innerRotation, fullRotations)
-
-    if (arrow.classList.contains("animate")) arrow.classList.remove("animate");
+    // arrow animation and result display
+    if (arrow.classList.contains("animate")) {
+        arrow.classList.remove("animate");
+    }
     setTimeout(() => {
+        if (inputs[panelIndex[1]].value !== "") {
+            result.innerHTML = `Spin result: <b>${inputs[panelIndex[1]].value}</b>`;
+            result.style.setProperty("color", panels[panelIndex[0]][0]);
+        } else {
+            result.innerHTML = `Spin result: <b>${panels[panelIndex[0]][0]}</b>`;
+            result.style.setProperty("color", panels[panelIndex[0]][0]);
+        }
         arrow.classList.add("animate");
-    }, 5000)
+    }, 5000);
 });
 
 for (let i = 0; i < inputs.length; i++) {
